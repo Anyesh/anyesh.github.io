4d0162b432198edf4d0dea291555e7e3aa446f92
Anyesh <sthacruz@gmail.com>
2020-08-02 18:34:04 +0545

    Align Fix

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4024ed040d7e0cf93b5d.js"
+    "url": "webpack-runtime-973c229268cb17c9c99d.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4749096e2a3dc4ddaefdac035d1c0610"
+    "revision": "39c6d4fc07adb554a77125de1e095d58"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f5aab79c5ddb506abb86baf04885f646"
+    "revision": "e16f57947b70c9c2d573b793d922b6dd"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
