c479c151759df6ef3c624b7feeddd6db7ec8f26f
Anyesh <sthacruz@gmail.com>
2020-08-04 09:43:44 +0545

    Fix color

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-aa1e62a436b7f9fe7ec7.js"
+    "url": "webpack-runtime-8b891243faa9685976e1.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "66f59deb9b67ad20d33523ac10232fb3"
+    "revision": "24483c7dee5781f49f13a67cd26cb6e5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7dcc3fc91ce2c43ddfd912c695d44db7"
+    "revision": "5c961ba702fc88cf2642a3920f8ef641"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
