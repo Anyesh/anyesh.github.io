96ef5b8f7510533a2a04083ca67b0e6fa11ef969
Anish Shrestha <sthacruz@gmail.com>
2026-07-31 19:26:52 +1000

    Point the lab's outbound links at www.anyesh.me

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const site = {
     "A small, growing collection of things I'm trying to understand, rebuilt as something you can poke at. Each piece started as an explanation and became a sandbox.",
   author: "Anish Shrestha",
   links: [
-    { label: "anyesh.me", href: "https://anyesh.me" },
-    { label: "github", href: "https://github.com/anyesh" },
+    { label: "anyesh.me", href: "https://www.anyesh.me" },
+    { label: "github", href: "https://github.com/Anyesh" },
   ],
 };
